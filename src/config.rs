0bc3ae6ba23434abd305426124127e3fdbe5a0d2
yyxxryrx <yyxxryrx@aliyun.com>
2025-09-02 20:31:59 +0800

    build: 更新项目版本号
    - 将 image_to_console 包的版本从 0.1.9 升级到 0.1.10

--- src/config.rs
+++ src/config.rs
@@ -32,7 +32,7 @@ pub const CLAP_STYLING: Styles = Styles::styled()
 
 #[derive(Parser)]
 #[clap(name = "image_to_console")]
-#[command(version = "0.1.9", color = clap::ColorChoice::Auto, styles = CLAP_STYLING)]
+#[command(version = "0.1.10", color = clap::ColorChoice::Auto, styles = CLAP_STYLING)]
 pub struct Cli {
     #[clap(short = 'c', long, help = "Center the image", default_value_t = false)]
     pub center: bool,
