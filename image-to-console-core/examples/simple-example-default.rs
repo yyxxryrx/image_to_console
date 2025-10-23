eb7e74e77fbaf966f4c2a6fe3d5b1021b85010f7
yyxxryrx <yyxxryrx@aliyun.com>
2025-10-23 20:43:56 +0800

    feat(examples): 添加默认选项显示图片的示例 - 创建简单示例展示如何使用默认选项处理并显示图片 - 实现从指定路径加载图片的功能 - 使用默认配置创建图像处理器 - 在控制台中打印处理后的图像结果

---
+++ image-to-console-core/examples/simple-example-default.rs
@@ -0,0 +1,21 @@
+use image::open;
+use image_to_console_core::processor::{ImageProcessor, ImageProcessorOptions};
+
+/// This is an example to use default option to display image
+fn main() {
+    // Open the image file from the specified path
+    let img = open(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/examples/images/flower.jpg"
+    ))
+    .expect("Cannot found image");
+
+    // Create default options for image processing
+    let option = ImageProcessorOptions::default();
+
+    // Process the image with the given options
+    let result = ImageProcessor::new(img, option).process();
+
+    // Display the processed image in the console
+    println!("{}", result.display());
+}
