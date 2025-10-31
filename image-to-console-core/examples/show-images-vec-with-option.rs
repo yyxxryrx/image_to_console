1eb080015cfbc73c58560ddaa894cb2fe29a05f7
yyxxryrx <yyxxryrx@aliyun.com>
2025-10-31 23:46:57 +0800

    feat(examples): 添加显示图像向量的示例代码
    - 新增 show-images-vec.rs 示例文件
    - 新增 show-images-vec-with-option.rs 示例文件
    - 展示如何使用 show_images! 宏显示图像向量
    - 展示如何使用选项配置图像处理器
    - 添加图像加载和错误处理逻辑
    - 提供带选项和不带选项的两种使用方式

---
+++ image-to-console-core/examples/show-images-vec-with-option.rs
@@ -0,0 +1,21 @@
+use image::open;
+use image_to_console_core::{processor::ImageProcessorOptions, show_images};
+
+fn main() {
+    let img1 = open(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/examples/images/flower.jpg"
+    ))
+        .expect("Cannot found image");
+    let img2 = open(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/examples/images/flower2.jpg"
+    ))
+        .expect("Cannot found image");
+    // Your images vector
+    let images = vec![img1, img2];
+    // Create options
+    let option = ImageProcessorOptions::default();
+    // Show images with options
+    show_images!(@vec images, @with_options option);
+}
---
+++ image-to-console-core/examples/show-images-vec.rs
@@ -0,0 +1,19 @@
+use image::open;
+use image_to_console_core::show_images;
+
+fn main() {
+    let img1 = open(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/examples/images/flower.jpg"
+    ))
+        .expect("Cannot found image");
+    let img2 = open(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/examples/images/flower2.jpg"
+    ))
+        .expect("Cannot found image");
+    // Your images vector
+    let images = vec![img1, img2];
+    // Show images
+    show_images!(@vec images);
+}
